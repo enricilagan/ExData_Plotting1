117d43f199a29a312faf55f1d39164b96a155c77
Enric Ilagan <enricilagan@gmail.com>
2016-06-17 22:36:26 +0800

    Commit

---
+++ plot2.R
@@ -0,0 +1,13 @@
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip","household_power_consumption.zip")
+unzip("household_power_consumption.zip")
+data <- read.table("household_power_consumption.txt", header=FALSE, sep=";", skip = grep("31/1/2007;23:59:00", readLines("household_power_consumption.txt")), nrows=2880)
+cols <- read.table("household_power_consumption.txt", header=TRUE, sep=";", nrows=1)
+data$V10 <- paste(data$V1, data$V2, sep=" ")
+data$V10 <- strptime(data$V10, format="%d/%m/%Y %H:%M:%S")
+data$V1 <- as.Date(data$V1, format="%d/%m/%Y")
+data$V2 <- strptime(data$V2, format="%H:%M:%S")
+names(data) <- c(names(cols),"datetime")
+
+png(file="plot2.png", bg = "transparent")
+with(data, plot(datetime, Global_active_power, type="l", xlab=" ", ylab = "Global Active Power (kilowatts)"))
+dev.off()
